b360d9843bcce5c6c3f0a574a7ee27afc5cc250a
Naomi Hsieh <nhsieh60@stuy.edu>
2024-09-27 17:59:12 -0400

    Fixed compliation errors, wrote another test case for returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -16,14 +16,21 @@ public class MyArrays{
     }
     return ary2;
   }
-  public static void main(){
+  public static void main(String[] args){
     int[] ary = {1, 2, 3};
-    System.out.println("Original:" + arrayToString(ary) + " Copy: " + arrayToString(returnCopy(ary))
-      + "Equals? " + arrayToString(ary).equals(arrayToString(returnCopy(ary))));
-    System.out.println("Original array address = Copy array address?" + returnCopy(ary) == ary);
-    int[] ary2 = {};
-    System.out.println("Original:" + arrayToString(ary2) + " Copy: " + arrayToString(returnCopy(ary2))
-      + "Equals? " + arrayToString(ary2).equals(arrayToString(returnCopy(ary2))));
-    System.out.println("Original array address = Copy array address?" + returnCopy(ary2) == ary2);
+    System.out.println("Original: " + arrayToString(ary) + " | Copy: " + arrayToString(returnCopy(ary))
+      + " | Equals? " + arrayToString(ary).equals(arrayToString(returnCopy(ary))));
+	System.out.println("Addresses: " + ary + ", " + returnCopy(ary) + " | Original array address = Copy array address? " + (returnCopy(ary) == ary));
+	
+    int[] ary2 = new int[0];
+	System.out.println("Original: " + arrayToString(ary2) + " | Copy: " + arrayToString(returnCopy(ary2))
+      + " | Equals? " + arrayToString(ary2).equals(arrayToString(returnCopy(ary2))));
+	System.out.println("Addresses: " + ary2 + ", " + returnCopy(ary2) + " | Original array address = Copy array address? " + (returnCopy(ary2) == ary2));
+	
+	int[] ary3 = {0, 1, -2, 3, 4, -2147483648, 2147483647};
+	System.out.println("Original: " + arrayToString(ary3) + " | Copy: " + arrayToString(returnCopy(ary3))
+      + " | Equals? " + arrayToString(ary3).equals(arrayToString(returnCopy(ary3))));
+	System.out.println("Addresses: " + ary3 + ", " + returnCopy(ary3) + " | Original array address = Copy array address? " + (returnCopy(ary3) == ary3));
+	
   }
 }
