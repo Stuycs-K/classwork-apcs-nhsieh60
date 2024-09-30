eefe46475088915e98816f75d8b529395f2638db
Naomi Hsieh <nhsieh60@stuy.edu>
2024-09-30 18:36:33 -0400

    Added one new test case for both arrToString and arr2DSum, added two new test cases for swapRC

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -58,6 +58,9 @@ public class ArrayMethods{
 	  ary1 = new int[][]{{6}, {4,4,4,4,4},{-5}};
       testString = "[[], [4, 4, 4, 4, 4], [-5]]";
       System.out.println("testString: " + testString + "Result: " + arrToString(ary1) + "Equals? " + testString.equals(arrToString(ary1)));
+	  ary1 = new int[][]{{7}, {8},{-5}};
+      testString = "[[], [4, 4, 4, 4, 4], [-5]]";
+      System.out.println("testString: " + testString + "Result: " + arrToString(ary1) + "Equals? " + testString.equals(arrToString(ary1)));
 
       ary1 = new int[][]{{1, 2}, {3, 4}};
       int sum = 10;
@@ -74,9 +77,18 @@ public class ArrayMethods{
 	  ary1 = new int[2][0];
       sum = 0;
       System.out.println("Expected sum: " + sum + "Result: " + arr2DSum(ary1) + "Equals? " + arr2DSum == sum);
+	  ary1 = new int[][]{{7}, {8},{-5}};
+	  sum = 10;
+	  System.out.println("Expected sum: " + sum + "Result: " + arr2DSum(ary1) + "Equals? " + arr2DSum == sum);
 	  
 	  ary1 = new int[][]{{1,2,3},{4,5,6}};
 	  testString = "[[1,4], [2, 5], [3, 6]]";
+	  System.out.println("Expected array: " + testString + "Result: " + arrToString(swapRC(ary1)) + "Equals? " + testString.equals(arrToString(ary1)));
+	  ary1 = new int[][]{{1,2},{4,5}};
+	  testString = "[[1, 4], [2, 5]]";
+	  System.out.println("Expected array: " + testString + "Result: " + arrToString(swapRC(ary1)) + "Equals? " + testString.equals(arrToString(ary1)));
+	  ary1 = new int[][]{{1, 2, 5, 6}, {4, 5, 7, 8}, {0, -1, -2147483648, 2147483647}};
+	  testString = "[[1, 4, 0], [2, 5, -1], [5, 7, -2147483648], [6, 8, 2147483647]]";
 	  System.out.println("Expected array: " + testString + "Result: " + arrToString(swapRC(ary1)) + "Equals? " + testString.equals(arrToString(ary1)));
 
     }
