7d1d53f53e0f4918e106751c6493de4228a492f2
Naomi Hsieh <nhsieh60@stuy.edu>
2024-10-15 09:55:28 -0500

    Added test cases

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -13,10 +13,25 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     Point p4 = new Point(p3);
+
     System.out.println(p3);
     System.out.println(p4 + " Is it the same point as p4? " + (p3 == p4));
-    System.out.println( distance(p1,p2));
+    System.out.println("distance between " + p1 + " and " + p2 + ": " + distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+
+    p2 = new Point(0,2);
+    p1  = new Point(3,6);
+    System.out.println("distance between " + p1 + " and " + p2 + ": " + distance(p1,p2));
+    System.out.println( Point.distance(p1,p2));
+    System.out.println( p1.distanceTo(p2));
+
+   p1 = new Point(-40,-10);
+   p2  = new Point(0,-1);
+   System.out.println("distance between " + p1 + " and " + p2 + ": " + distance(p1,p2));
+   System.out.println( Point.distance(p1,p2));
+   System.out.println( p1.distanceTo(p2));
+
+
   }
 }
