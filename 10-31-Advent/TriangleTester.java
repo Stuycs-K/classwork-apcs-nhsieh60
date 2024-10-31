58f36e18afeb8135598633db97dcc3b3943632b0
Naomi Hsieh <nhsieh60@stuy.edu>
2024-10-31 10:14:03 -0500

    wrote part A

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -3,6 +3,18 @@ public class TriangleTester{
     File file = new File(filename);
     Scanner triangles = new Scanner(file);
 
-    
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int count = 0;
+    while(triangles.hasNextLine()){
+      a = triangles.nextInt();
+      b = triangles.nextInt();
+      c = triangles.nextInt();
+      if(a + b >= c && a + c >= b && b + c >= a){
+        count++;
+      }
+    }
+  return count;
   }
 }
