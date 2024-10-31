8f4d8495b83c104b5ac80f4be2184c9c88dfc9de
Naomi Hsieh <nhsieh60@stuy.edu>
2024-10-31 18:53:29 -0400

    changed function to have a helper function to test triangle

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,20 +1,25 @@
 public class TriangleTester{
-  public static int countTrianglesA(String filename){
-    File file = new File(filename);
-    Scanner triangles = new Scanner(file);
+	
+	public static boolean triangleTest(int a, int b, int c){
+		return a + b >= c && a + c >= b && b + c >= a; 
+	}
+	
+	public static int countTrianglesA(String filename){
+		File file = new File(filename);
+		Scanner triangles = new Scanner(file);
 
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int count = 0;
-    while(triangles.hasNextLine()){
-      a = triangles.nextInt();
-      b = triangles.nextInt();
-      c = triangles.nextInt();
-      if(a + b >= c && a + c >= b && b + c >= a){
-        count++;
-      }
-    }
-  return count;
-  }
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int count = 0;
+		while(triangles.hasNextLine()){
+			a = triangles.nextInt();
+			b = triangles.nextInt();
+			c = triangles.nextInt();
+			if(triangleTest(a,b,c)){
+				count++;
+			}
+		}
+		return count;
+	}
 }
