803b3faeae7e3da139c58baee642d0345e4073d6
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-07 09:47:25 -0600

    overrode bird get name and answered driver question

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,5 @@
+/* The difference is that after overriding, when speak is called, it prints out "My name is The Mighty Parrot" instead of just
+"My name is Parrot" as speak is calling and using what the overriden getName() returned*/
 public class Driver{
   public static void main(String[] args){
     Animal water = new Animal("splash", 12, "Water");
