51e795af667257f6e48d6bef9764d98ac9d17f4d
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-07 10:00:22 -0600

    tested speak for new animals

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,8 +2,8 @@
 it prints out "My name is The Mighty Parrot" instead of just "My name is Parrot"
 as speak is calling and using what the overriden getName() returned*/
 /*Bird b2 = new Animal(...); didn't work because Bird is not the superclass of Animal
-(Animal is-a Bird is not true). Animal a2 = new Bird(...); worked because Animal is
-the superclass of Bird, thus Bird is-a Animal is true*/
+(the Animal is-a Bird relationship is not true). Animal a2 = new Bird(...); worked because Animal is
+the superclass of Bird, thus Bird is-a Animal and you can store bird in an animal*/
 public class Driver{
   public static void main(String[] args){
     Animal water = new Animal("splash", 12, "Water");
@@ -16,5 +16,8 @@ public class Driver{
     Bird b1 = new Bird("quack", 10, "duck", 2, "white");
   //  Bird b2 = new Animal("swish", 190, "jellyfish");
     Animal a2 = new Bird("quack", 10, "duck", 2, "white");
+    a1.speak();
+    a2.speak();
+    b1.speak();
   }
 }
