d935affce2fe8cb2ee3885f3bbbbddc9a60f99b1
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-07 09:35:45 -0600

    wrote animal constructor and speak and getage

--- 2024-11-07-Pets/Animal.java
+++ 2024-11-07-Pets/Animal.java
@@ -5,6 +5,9 @@ public class Animal {
 
   public Animal(String noise, int age, String name){
     //initialize the values (hint use this.varname to clarify)
+    this.noise = noise;
+    this.age = age;
+    this.name = name;
   }
 
   //2.
@@ -17,7 +20,9 @@ public class Animal {
   Note: you MUST use getName() getNoise() here! You will see why later.
   */
   public void speak(){
-
+    System.out.println("My name is " + getName() + ".");
+    System.out.println("I am " + getAge() + " years old.");
+    System.out.println("I say " + getNoise() + ".");
   }
 
   public String getName(){
@@ -25,5 +30,8 @@ public class Animal {
   }
   public String getNoise(){
     return noise;
+  }
+  public String getAge(){
+    return age;
   }
 }
