3a6a53aeed68b336dc79c83d570f52eda83f4327
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-18 23:44:13 -0500

    debugged DayOne, passed DayOne

--- 2024-11-18-advent2016/DayOne.java
+++ 2024-11-18-advent2016/DayOne.java
@@ -12,48 +12,61 @@ public class DayOne{
 		Scanner directions = new Scanner(file);
 		int x = 0;
 		int y = 0;
-		int i = 0;
-		String prevDirection = "L";
+		String prevDirection = "N";
 		String curDirection = "";
 		int steps = 0;
 		while(directions.hasNext()){
+			System.out.println("Previous Direction: " + prevDirection);
+			System.out.println("Previous coordinates: (" + x + ", " + y + ")");
 			curDirection = directions.next();
-			steps = Integer.parseInt(curDirection.substring(1, 2));
+			if(curDirection.charAt(curDirection.length()-1) == ','){
+				curDirection = curDirection.substring(0, curDirection.length()-1);
+			}
+			steps = Integer.parseInt(curDirection.substring(1));
 			curDirection = curDirection.substring(0,1);
-			if(prevDirection.equals("R") && i % 2 == 0){
+			if(prevDirection.equals("S")){
 				if(curDirection.equals("R")){
 					x -= steps;
+					prevDirection = "W";
 				}
 				else{
 					x += steps;
+					prevDirection = "E";
 				}
 			}
-			else if(prevDirection.equals("R") && i % 2 != 0){
+			else if(prevDirection.equals("W")){
 				if(curDirection.equals("R")){
-					y -= steps;
-				}
-				else{
 					y += steps;
+					prevDirection = "N";
+				}
+				else{
+					y -= steps;
+					prevDirection = "S";
+					
 				}
 			}
-			else if(prevDirection.equals("L") && i % 2 == 0){
+			else if(prevDirection.equals("N")){
 				if(curDirection.equals("R")){
 					x += steps;
+					prevDirection = "E";
 				}
 				else{
 					x -= steps;
+					prevDirection = "W";
 				}
 			}
 			else{
 				if(curDirection.equals("R")){
-					y += steps;
+					y -= steps;
+					prevDirection = "S";
 				}
 				else{
-					y -= steps;
+					y += steps;
+					prevDirection = "N";
 				}
 			}
-			i++;
-			prevDirection = curDirection;
+			System.out.println("Given directions: " + curDirection + steps);
+			System.out.println("Current coordinates: (" + x + ", " + y + ")\n");
 		}
 		directions.close();
 		return Math.abs(x)+Math.abs(y);
