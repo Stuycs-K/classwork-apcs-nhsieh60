8ae3fccd4f0df2bb0a1d1c4c085015d2fcd810db
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-19 11:01:28 -0500

    continued implementation of day4, parsing room code now

--- 2024-11-18-advent2016/DayFour.java
+++ 2024-11-18-advent2016/DayFour.java
@@ -7,11 +7,16 @@ public class DayFour{
       Scanner input = new Scanner(new File(filename));
       String[] room = "";
       int[] alphabetCount = new int[26];
+      int sum = 0;
+      String[] roomCode = new String[5];
       while(input.hasNextLine()){
         room = input.nextLine.split("-");
         for(int i = 0; i < room.length-1; i++){
-          if(room[i])
+          for(int j = 0; j < room[i].length(); j++){
+            alphabetCount[room[i]-97]++;
+          }
         }
+        for(int i = 0; i <)
       }
     }
     catch(FileNotFoundException e){
