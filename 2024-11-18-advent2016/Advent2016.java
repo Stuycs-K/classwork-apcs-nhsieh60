b4f6f2d052c7fc4048efa390dbaa098b8d53f649
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-19 00:57:29 -0500

    wrote day one part two

--- 2024-11-18-advent2016/Advent2016.java
+++ 2024-11-18-advent2016/Advent2016.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Advent2016{
   public static void main(String[] args){
@@ -77,6 +78,87 @@ public class Advent2016{
 		return 0;
 	}
   }
+  
+   public static int dayOnePartTwo(String filename){
+	try{
+		File file = new File(filename);
+		Scanner directions = new Scanner(file);
+		int x = 0;
+		int y = 0;
+		String prevDirection = "N";
+		String curDirection = "";
+		int steps = 0;
+		ArrayList<Integer> coordinates = new ArrayList();
+		while(directions.hasNext()){
+			//System.out.println("Previous Direction: " + prevDirection);
+			//System.out.println("Previous coordinates: (" + x + ", " + y + ")");
+			curDirection = directions.next();
+			if(curDirection.charAt(curDirection.length()-1) == ','){
+				curDirection = curDirection.substring(0, curDirection.length()-1);
+			}
+			steps = Integer.parseInt(curDirection.substring(1));
+			curDirection = curDirection.substring(0,1);
+			if(prevDirection.equals("S")){
+				if(curDirection.equals("R")){
+					x -= steps;
+					prevDirection = "W";
+				}
+				else{
+					x += steps;
+					prevDirection = "E";
+				}
+			}
+			else if(prevDirection.equals("W")){
+				if(curDirection.equals("R")){
+					y += steps;
+					prevDirection = "N";
+				}
+				else{
+					y -= steps;
+					prevDirection = "S";
+					
+				}
+			}
+			else if(prevDirection.equals("N")){
+				if(curDirection.equals("R")){
+					x += steps;
+					prevDirection = "E";
+				}
+				else{
+					x -= steps;
+					prevDirection = "W";
+				}
+			}
+			else{
+				if(curDirection.equals("R")){
+					y -= steps;
+					prevDirection = "S";
+				}
+				else{
+					y += steps;
+					prevDirection = "N";
+				}
+			}
+			//System.out.println("Given directions: " + curDirection + steps);
+			//System.out.println("Current coordinates: (" + x + ", " + y + ")\n");
+			for(int i = 0; i < coordinates.size(); i+=2){
+				if(coordinates.get(i) == x && coordinates.get(i+1) == y){
+					return x + y;
+				}
+			}
+			coordinates.add(x);
+			coordinates.add(y);
+		}
+		directions.close();
+		return Math.abs(x)+Math.abs(y);
+	}
+	catch(FileNotFoundException e){
+		System.out.println("File not found");
+		return 0;
+	}
+  }
+  
+  
   public static String dayTwo(String filename){
 	  try{
 		File file = new File(filename);
