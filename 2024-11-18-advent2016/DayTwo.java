b9d90bea9219ec9e5fadd781dee1e52a025ccaba
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-19 20:14:13 -0500

    wrote first rendition of day two part two

--- 2024-11-18-advent2016/DayTwo.java
+++ 2024-11-18-advent2016/DayTwo.java
@@ -46,4 +46,42 @@ public class DayTwo{
 	}
   }
   
+  public static String partTwo(String filename){
+	  try{
+		File file = new File(filename);
+		Scanner directions = new Scanner(file);
+		String[][] keypad = new int[][]{{"", "", "1", "", ""}, {"", "2", "3", "4", ""}, {"5", "6", "7", "8", "9"}, {"", "A", "B", "C", ""}, {"", "", "D", "", ""}};
+		String code = "";
+		String curDirections = "";
+		int x = 1;
+		int y = 1;
+		while(directions.hasNextLine()){
+			curDirections = directions.nextLine();
+			//System.out.println(curDirections);
+			//System.out.println(keypad[x][y]);
+			for(int i = 0; i < curDirections.length(); i++){
+				if(curDirections.charAt(i) == 'U' && y > 0 && keypad[y-1][x] != ""){
+					y -= 1;
+				}
+				if(curDirections.charAt(i) == 'D' && y < 2 && keypad[y+1][x] != ""){
+					y += 1;
+				}
+				if(curDirections.charAt(i) == 'L' && x > 0 && keypad[y][x-1] != ""){
+					x -= 1;
+				}
+				if(curDirections.charAt(i) == 'R' && x < 2 && && keypad[y][x+1] != ""){
+					x += 1;
+				}
+				//System.out.println(keypad[y][x]);
+			}
+			code += keypad[y][x];
+		}
+		return code;
+	}
+	catch(FileNotFoundException e){
+		System.out.println("File not found");
+		return "";
+	}
+  }
+  
 }
