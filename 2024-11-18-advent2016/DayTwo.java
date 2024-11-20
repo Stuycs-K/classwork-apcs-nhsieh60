971a9595cdb873a62eb4095e2d8fa3f2b98ff5ec
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-19 20:19:10 -0500

    debugged and ran day two part two

--- 2024-11-18-advent2016/DayTwo.java
+++ 2024-11-18-advent2016/DayTwo.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class DayTwo{
 	public static void main(String[] args){
 		System.out.println(partOne("DayTwoInput.txt"));
+		System.out.println(partTwo("DayTwoInput.txt"));
 	}
 	
 	public static String partOne(String filename){
@@ -50,11 +51,11 @@ public class DayTwo{
 	  try{
 		File file = new File(filename);
 		Scanner directions = new Scanner(file);
-		String[][] keypad = new int[][]{{"", "", "1", "", ""}, {"", "2", "3", "4", ""}, {"5", "6", "7", "8", "9"}, {"", "A", "B", "C", ""}, {"", "", "D", "", ""}};
+		String[][] keypad = new String[][]{{"", "", "1", "", ""}, {"", "2", "3", "4", ""}, {"5", "6", "7", "8", "9"}, {"", "A", "B", "C", ""}, {"", "", "D", "", ""}};
 		String code = "";
 		String curDirections = "";
-		int x = 1;
-		int y = 1;
+		int x = 0;
+		int y = 2;
 		while(directions.hasNextLine()){
 			curDirections = directions.nextLine();
 			//System.out.println(curDirections);
@@ -63,17 +64,18 @@ public class DayTwo{
 				if(curDirections.charAt(i) == 'U' && y > 0 && keypad[y-1][x] != ""){
 					y -= 1;
 				}
-				if(curDirections.charAt(i) == 'D' && y < 2 && keypad[y+1][x] != ""){
+				if(curDirections.charAt(i) == 'D' && y < 4 && keypad[y+1][x] != ""){
 					y += 1;
 				}
 				if(curDirections.charAt(i) == 'L' && x > 0 && keypad[y][x-1] != ""){
 					x -= 1;
 				}
-				if(curDirections.charAt(i) == 'R' && x < 2 && && keypad[y][x+1] != ""){
+				if(curDirections.charAt(i) == 'R' && x < 4 && keypad[y][x+1] != ""){
 					x += 1;
 				}
 				//System.out.println(keypad[y][x]);
 			}
+			//System.out.println("Final: " + keypad[y][x]);
 			code += keypad[y][x];
 		}
 		return code;
