0c479359893af380293312b6ad97b3782263a800
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-20 11:00:50 -0500

    debugged dayseven have not run yet

--- 2024-11-18-advent2016/DaySeven.java
+++ 2024-11-18-advent2016/DaySeven.java
@@ -8,23 +8,30 @@ public class DaySeven{
 		System.out.println(partOne("DaySevenInput.txt"));
 	}
 
-	public static String partOne(String filename){
+	public static int partOne(String filename){
 	  try{
 		File file = new File(filename);
 		Scanner ips = new Scanner(file);
     String curIP = "";
     int count = 0;
+    boolean contains = false;
     while(ips.hasNextLine()){
       curIP = ips.nextLine();
+      System.out.println(curIP);
       for(int i = 0; i < curIP.length()-3; i++){
-        if(curIP.charAt(i) == curIP.charAt(i+3) && curIP.charAt(i+1) == curIP.charAt(i+2)){
-          count++;
+        if(curIP.charAt(i) == curIP.charAt(i+3) && curIP.charAt(i+1) == curIP.charAt(i+2) && curIP.charAt(i+1) != curIP.charAt(i)){
+          contains = true;
+          System.out.println(curIP.substring(i, i+4));
         }
       }
-      int iBracket = curIP.indexOf("[")
-      if(curIP.charAt(iBracket+1) == curIP.charAt(iBracket+4) && curIP.charAt(iBracket+2) == curIP.charAt(iBracket+3)){
-        count--;
+      int iBracket = curIP.indexOf("[");
+      System.out.println(count);
+      if(!(curIP.charAt(iBracket+1) == curIP.charAt(iBracket+4) && curIP.charAt(iBracket+2) == curIP.charAt(iBracket+3) && curIP.charAt(iBracket+1) != curIP.charAt(iBracket+2)) && contains){
+        count++;
+        System.out.println(curIP.substring(iBracket, iBracket+6));
       }
+      System.out.println(count);
+      contains = false;
     }
     ips.close();
     return count;
@@ -32,6 +39,7 @@ public class DaySeven{
 	}
 	catch(FileNotFoundException e){
 		System.out.println("File not found");
-		return "";
+		return 0;
 	}
   }
+}
