b11edc1650f5a9fc9fdb92be0eb596fceb34809b
Naomi Hsieh <nhsieh60@stuy.edu>
2024-11-19 19:51:31 -0500

    finished writing DayFour and tested with input

--- 2024-11-18-advent2016/DayFour.java
+++ 2024-11-18-advent2016/DayFour.java
@@ -10,19 +10,70 @@ public class DayFour{
   public static int partOne(String filename){
     try{
       Scanner input = new Scanner(new File(filename));
-      String[] room = "";
+      String[] room = new String[1];
       int[] alphabetCount = new int[26];
       int sum = 0;
-      String[] roomCode = new String[5];
+     // String[] roomCode = new String[]{" 0", " 0", " 0", " 0", " 0"};
+	  ArrayList<String> roomCode = new ArrayList<String>(26);
+	  roomCode.add("a0");
+	  boolean placed = false;
+	  int k = 0;
+	 // int l = 0;
       while(input.hasNextLine()){
-        room = input.nextLine.split("-");
+        room = input.nextLine().split("-");
         for(int i = 0; i < room.length-1; i++){
           for(int j = 0; j < room[i].length(); j++){
-            alphabetCount[room[i]-97]++;
+            alphabetCount[room[i].charAt(j)-97]++;
           }
         }
-        for(int i = 0; i <)
+        for(int i = 0; i < alphabetCount.length; i++){
+			while(k < roomCode.size() && alphabetCount[i] <= Integer.parseInt(roomCode.get(k).substring(1))){
+			//	System.out.println("run");
+				k++;
+			}
+			roomCode.add(k, "" + ((char)(i+97)) + alphabetCount[i]);
+			k = 0;
+			/*while(!placed && k < roomCode.length){
+				if(alphabetCount[i] > Integer.parseInt(roomCode[k].substring(1))){
+					l = k;
+					while(!placed && l < roomCode.length){
+						if(Integer.parseInt(roomCode[k].substring(1)) > Integer.parseInt(roomCode[l].substring(1))){
+							roomCode[l] = roomCode[k];
+							placed = true;
+						}
+						l++;
+					}
+					roomCode[k] = "" + ((char)(i+97)) + alphabetCount[i];
+					placed = true;
+					System.out.println(roomCode[k]);
+				}
+				k++;
+			}
+			placed = false;
+			k = 0;*/
+		}
+		String lastSegment = room[room.length-1];
+		String matchCode = lastSegment.substring(lastSegment.indexOf("[")+1, lastSegment.length()-1);
+		String id = lastSegment.substring(0, lastSegment.indexOf("["));
+		boolean matching = true;
+		for(int i = 0; i < 5; i++){
+			if(matchCode.charAt(i) != roomCode.get(i).charAt(0)){
+				matching = false;
+			}
+			System.out.print(roomCode.get(i).charAt(0));
+		}
+		System.out.println(" " + matchCode);
+		System.out.println(id);
+		if(matching){
+			sum += Integer.parseInt(id);
+		}
+		System.out.println(sum);
+		matching = true;
+		alphabetCount = new int[26];
+		//roomCode = new String[]{" 0", " 0", " 0", " 0", " 0"};
+		roomCode = new ArrayList<String>(26);
       }
+	  return sum;
     }
     catch(FileNotFoundException e){
       System.out.println("FILE NOT FOUND");
