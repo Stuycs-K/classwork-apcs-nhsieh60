513f7a5952164c1cd6c23a60dfd05345540e0241
Naomi Hsieh <nhsieh60@stuy.edu>
2024-12-04 18:19:46 -0500

    added clearscreen before generating border because terminal was getting too cramped, wrote generating/placing three random numbers

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -19,8 +19,31 @@ public class Driver{
 			System.out.print(" ");
 		}
 	}
+	public static void numbers(){
+		int[] arr = new int[3];
+		int increment = 1;
+		for(int i = 0; i < 3; i++){
+			arr[i] = (int)(Math.random()*100);
+			increment += 19;
+			Text.go(2, increment);
+			if(arr[i] < 25){
+				Text.color(91);
+				System.out.println(arr[i] + Text.RESET);
+			}
+			else if(arr[i] > 75){
+				Text.color(92);
+				System.out.println(arr[i] + Text.RESET);
+			}
+			else{
+				Text.color(37);
+				System.out.println(arr[i] + Text.RESET);
+			}
+		}
+	}
 	public static void main(String[] args){
+		System.out.println(Text.CLEAR_SCREEN);
 		border((int)(Math.random()*7)+31);
+		numbers();
 		System.out.println(Text.RESET);
 	}
 }
