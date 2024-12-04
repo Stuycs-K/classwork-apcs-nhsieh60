1fb670624bedc82c22cff3766056e8899bcac8b4
Naomi Hsieh <nhsieh60@stuy.edu>
2024-12-03 21:03:02 -0500

    escape command is not working on my terminal at the moment, so i am testing with letters and will switch to escape when debugged; coded how to print out first half of diamond/circle

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -2,6 +2,24 @@ public class ColorDemo{
   public static final String CLEAR_SCREEN =  "\u001b[2J";
   public static final String HIDE_CURSOR =  "\u001b[?25l";
   public static final String SHOW_CURSOR =  "\u001b[?25h";
+  public static void makeBubble(int n){
+	int whitespace = n-4;
+	int counter = 4;
+	for(int i = 4; i < n; i++){
+		for(int j = 0; j < whitespace; j++){
+			System.out.print("W");
+		}
+		for(int j = 0; j < counter; j++){
+			System.out.print("B");
+		}
+		for(int j = 0; j < whitespace; j++){
+			System.out.print("W");
+		}
+		System.out.println();
+		whitespace--;
+		counter += 2;
+	}
+  }
 
   public static void main(String[] args){
     for(int b = 0; b < 256; b+=32){
@@ -15,6 +33,6 @@ public class ColorDemo{
     }
       System.out.println(" \u001b[0m I have reset the colors.");
       System.out.println("\u001b[1;4;38;2;200;10;255m lots of settings here");
+	  makeBubble(7);
   }
-
 }
