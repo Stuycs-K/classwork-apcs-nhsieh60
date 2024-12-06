f30ada6edfde453b66176b9b6e1d8435a984efcd
Naomi Hsieh <nhsieh60@stuy.edu>
2024-12-06 10:39:21 -0500

    wrote constructors and some abstract methods

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -1,3 +1,33 @@
-public class Mage{
+public class Mage extends Adventurer{
+
+  private int mana, maxMana;
+  private String name;
+  private int HP, maxHP;
+
+  public Mage(String name){
+    super(name);
+  }
+
+  public Mage(String name, int hp){
+    super(name, hp);
+    int mana = 10;
+    int maxMana = 10;
+  }
+
+  public String getSpecialName(){
+    return "mana";
+  }
+
+  public int getSpecial(){
+    return mana;
+  }
+
+  public int getSpecialMax(){
+    return maxMana;
+  }
   
+  public void setSpecial(int n){
+    this.mana = n;
+  }
+
 }
