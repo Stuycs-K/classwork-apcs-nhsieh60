d99308d701a91bd668b23347e38ba8164cedd1f5
Naomi Hsieh <nhsieh60@stuy.edu>
2025-01-03 11:01:02 -0500

    wrote makeWords and debugged

---
+++ 2025-01-02/Words.java
@@ -0,0 +1,17 @@
+public class Words{
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if(remainingLetters==0){
+      System.out.println(result);
+    }
+    else{
+      for(int i = 0; i < alphabet.length(); i++){
+        makeWords(remainingLetters-1, result+alphabet.charAt(i), alphabet);
+      }
+    }
+  }
+
+  public static void main(String[] args){
+    makeWords(2, "", "abc");
+    makeWords(3, "", "xy");
+  }
+}
